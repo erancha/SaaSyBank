ed80faf9eb7a1deb06a3e3c95734f6bccbf455c5
Eran Hachmon <erancha@gmail.com>
2024-11-13 14:10:00 +0200

    Minor fixes in scripts\dev-build-deploy-delete.ps1

--- scripts/dev-build-deploy-delete.ps1
+++ scripts/dev-build-deploy-delete.ps1
@@ -8,33 +8,26 @@ $startTime = Get-Date
 
 if (-not $skipBuild) {
    ./dev-build-deploy.ps1
+
+   $formattedElapsedTime = Get-ElapsedTimeFormatted -startTime $startTime
+   Write-Output "`n$(Get-Date -Format 'HH:mm:ss'), elapsed $formattedElapsedTime : Testing all HTTP requests deployed by the stack ..."
+   ./test-web-api.ps1 -parallelCount 5 -iterationsCount 10
 }
-
-$commonConstants = ./common-constants.ps1
-
-# $formattedElapsedTime = Get-ElapsedTimeFormatted -startTime $startTime
-# Write-Output "`n$(Get-Date -Format 'HH:mm:ss'), elapsed $formattedElapsedTime : Listing ECS clusters, services, tasks and containers ..."
-# ./list-ECS.ps1 -region $commonConstants.region
-
-# $formattedElapsedTime = Get-ElapsedTimeFormatted -startTime $startTime
-# Write-Output "`n$(Get-Date -Format 'HH:mm:ss'), elapsed $formattedElapsedTime : Listing all subnets ..."
-# ./list-Subnets.ps1 -region $commonConstants.region
-
-Write-Output "`n$(Get-Date -Format 'HH:mm:ss'), elapsed $formattedElapsedTime : Testing all HTTP requests deployed by the stack ..."
-./test-web-api.ps1 -parallelCount 5 -iterationsCount 10
 
 if (-not $deleteTask) {
    $validInput = $false
    while (-not $validInput) {
-       $message = "Stack deployed. Continue to [d]elete the Stack or E[x]it ?"
+       $formattedElapsedTime = Get-ElapsedTimeFormatted -startTime $startTime
+       $message = "`n$(Get-Date -Format 'HH:mm:ss'), elapsed $formattedElapsedTime : Stack deployed. Continue to [d]elete the Stack or E[x]it ?"
        Write-Host $message  -ForegroundColor White -BackgroundColor DarkGray
        $userInput = Read-Host ">"
+       $formattedElapsedTime = Get-ElapsedTimeFormatted -startTime $startTime
 
        if ($userInput -eq 'x' -or $userInput -eq 'X') {
-           Write-Host "Exiting the script."
+           Write-Host "$(Get-Date -Format 'HH:mm:ss'), elapsed $formattedElapsedTime : Exiting the script."
            exit
        } elseif ($userInput -eq 'd' -or $userInput -eq 'D') {
-           Write-Host "Continuing to delete the stack..."
+           Write-Host "$(Get-Date -Format 'HH:mm:ss'), elapsed $formattedElapsedTime : Continuing to delete the stack..."
            $deleteTask = $true
            $validInput = $true
        } else {
@@ -44,18 +37,12 @@ if (-not $deleteTask) {
 }
 
 if ($deleteTask) {
+   $commonConstants = ./common-constants.ps1
+
    $formattedElapsedTime = Get-ElapsedTimeFormatted -startTime $startTime
    Write-Output "`n$(Get-Date -Format 'HH:mm:ss'), elapsed $formattedElapsedTime : Deleting stack '$($commonConstants.stackName)' ..."
    aws cloudformation delete-stack --stack-name $commonConstants.stackName
    aws cloudformation wait stack-delete-complete --stack-name $commonConstants.stackName
-
-   $formattedElapsedTime = Get-ElapsedTimeFormatted -startTime $startTime
-   Write-Output "`n$(Get-Date -Format 'HH:mm:ss'), elapsed $formattedElapsedTime : Listing ECS clusters, services, tasks and containers ..."
-   ./list-ECS.ps1 -region $commonConstants.region
-
-   $formattedElapsedTime = Get-ElapsedTimeFormatted -startTime $startTime
-   Write-Output "`n$(Get-Date -Format 'HH:mm:ss'), elapsed $formattedElapsedTime : Listing all subnets ..."
-   ./list-Subnets.ps1 -region $commonConstants.region
 }
 
 $formattedElapsedTime = Get-ElapsedTimeFormatted -startTime $startTime
